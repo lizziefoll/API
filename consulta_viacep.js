fd89354841f96d4c47c09657198975bb74470c8b
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-08-14 09:01:22 -0300

    Update consulta_viacep.js

---
+++ consulta_viacep.js
@@ -0,0 +1,60 @@
+function limparFormulario(endereco){
+    document.getElementById("endereco").value = ""; 
+    document.getElementById("bairro").value = ""; 
+    document.getElementById("cidade").value = ""; 
+    document.getElementById("estado").value = ""; 
+    document.getElementById("ibge").value = ""; 
+    document.getElementById("ddd").value = ""; 
+    document.getElementById("siafi").value = ""; 
+    document.getElementById("uf").value = ""; 
+}
+
+
+function preencherFormulario(endereco){
+    document.getElementById("endereco").value = endereco.logradouro; 
+    document.getElementById("bairro").value = endereco.bairro; 
+    document.getElementById("cidade").value = endereco.localidade; 
+    document.getElementById("estado").value = endereco.uf;
+    document.getElementById("ibge").value = endereco.uf; 
+    document.getElementById("ddd").value = endereco.ddd; 
+    document.getElementById("siafi").value = endereco.siafi; 
+    document.getElementById("uf").value = endereco.uf; 
+}
+
+
+function eNumero(numero){
+    return /^[0-9]+$/.test(numero); 
+
+}
+
+function cepValido(cep){
+    return cep.length == 8 && eNumero(cep); 
+
+}
+
+
+async function pesquisarCep(){
+    limparFormulario(endereco); 
+
+    const cep = document.getElementById("cep").value.replace("-", ""); 
+    console.log(cep); 
+    const url = `https://viacep.com.br/ws/${cep}/json/`; 
+
+    if(cepValido(cep)){
+        const dados = await fetch(url); 
+        console.log(dados); 
+        const endereco = await dados.json(); 
+        console.log(endereco); 
+
+        if(endereco.hasOwnProperty("erro")){
+            document.getElementById("endereco").value = "CEP não encontrado"; 
+        }else{
+            preencherFormulario(endereco); 
+        }
+    } else{
+    document.getElementById("endereco").value = "CEP incorreto"; 
+    }
+}
+
+document.getElementById("cep")
+    .addEventListener("focusout", pesquisarCep); 
